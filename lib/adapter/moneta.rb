ea734c2a61d87a09ec6c6232105706fd62ea5434
Luke Chadwick <me@vertis.io>
2014-01-08 09:19:40 +1100

    Generate a new gem

---
+++ lib/adapter/moneta.rb
@@ -0,0 +1,7 @@
+require "adapter/moneta/version"
+
+module Adapter
+  module Moneta
+    # Your code goes here...
+  end
+end
---
+++ lib/adapter/moneta/version.rb
@@ -0,0 +1,5 @@
+module Adapter
+  module Moneta
+    VERSION = "0.0.1"
+  end
+end
