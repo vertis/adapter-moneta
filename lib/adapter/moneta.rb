a2bb365de201f78e5a008e5fbfcd348e2996d838
Luke Chadwick <me@vertis.io>
2014-01-08 09:40:04 +1100

    Add the ultrabasic moneta adapter

--- lib/adapter/moneta.rb
+++ lib/adapter/moneta.rb
@@ -1,7 +1,20 @@
-require "adapter/moneta/version"
+require 'moneta'
+require 'adapter'
 
-module Adapter
-  module Moneta
-    # Your code goes here...
+Adapter.define(:moneta) do
+  def read(key, options = nil)
+    client[key]
+  end
+
+  def write(key, attributes, options = nil)
+    client[key] = attributes
+  end
+
+  def delete(key, options = nil)
+    client.delete(key)
+  end
+
+  def clear(options = nil)
+    client.clear
   end
 end
